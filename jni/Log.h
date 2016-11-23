844c293c796775ac031038848d482f923f32e40c
marcocorvi <marco.corvi@gmail.com>
2016-11-23 21:58:02 +0100

    update

--- jni/Log.h
+++ jni/Log.h
@@ -3,7 +3,7 @@
 
 #include <android/log.h>
 
-#define TAG "Native"
+#define TAG "CaveGL"
 
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__))
 #define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__))
